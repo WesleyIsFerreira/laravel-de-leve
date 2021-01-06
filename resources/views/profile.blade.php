793135daae223d6735d9b96bd4ee366607bd4e7d
Wesley Ferreira <37143147+WesleyIsFerreira@users.noreply.github.com>
2021-01-06 09:58:26 -0300

    Criando rotas
    Tambem foi inserido o @SRF na rota 'insere'

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>PROFILE</h1>
+    <div>
+        <a href="{{route('app')}}">app</a><br>
+        <a href="{{route('app.profile')}}">profile</a><br>
+        <a href="{{route('app.user')}}">user</a>
+    </div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+//use GuzzleHttp\Psr7\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +14,79 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+//DIRECIONA PRA VIEW RAIZ
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/teste', function(){
+    return "chama";
+})->name('teste');
+
+//PASSANDO PARAMETROS NA URL
+Route::get('/teste2/{nome}/{sobrenome}', function($nome, $sobrenome){
+    return "chama lá o  $nome $sobrenome ai pf";
+});
+
+//PARAMETROS NÃO OBRIGATÓRIOS
+Route::get('/seunome/{nome?}', function($nome=null){
+    if ($nome)
+        return "chama lá o  $nome ai pf";
+    return "Digia a rota certo jão!!!";
+});
+
+//EXPRESSÕES REGULARES PARA CHECAR PARAMETROS
+Route::get('/rotacomregras/{nome}/{num}', function($nome, $num){
+
+    for($i = 0; $i < $num; $i++)
+        echo ($i + 1) ."° vez do " . $nome ."<br>";
+    
+})->where('nome', '[A-Za-z]+')->where('num','[0-9]+');
+
+//AGRUPANDO ROTAS E NOMEANDO
+Route::prefix('aplicacao')->group(function () {
+
+    Route::get('/', function () {
+        return view('app');
+    })->name('app');
+
+    Route::get('/profile', function () {
+        return view('profile');
+    })->name('app.profile');
+
+    Route::get('/user', function () {
+        return view('user');
+    })->name('app.user');
+
+});
+
+//REDIRECIONANDO
+Route::redirect('todosprodutos', 'teste', 301);
+
+
+Route::get('todosprodutos2', function() {
+    return redirect()->route('teste');
+});
+
+////////////////////////////////////////////
+
+
+Route::post('/insere', function(Request $request) {
+    return "Inserido com sucesso";
+});
+Route::delete('/insere', function(Request $request) {
+    return "Deletado com sucesso";
+});
+Route::put('/insere', function(Request $request) {
+    return "Atualizado com sucesso";
+});
+Route::patch('/insere', function(Request $request) {
+    return "Atualizado no patch com sucesso";
+});
+Route::options('/insere', function(Request $request) {
+    return "Options no patch com sucesso";
+});
+Route::get('/insere', function(Request $request) {
+    return "getzada com sucesso";
+});
+
